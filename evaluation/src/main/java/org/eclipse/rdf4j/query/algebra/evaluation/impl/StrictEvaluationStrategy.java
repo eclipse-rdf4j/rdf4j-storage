09cd5b2725167d82b05e02b8a1661c365d636f20
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-07-28 11:41:13 +1000

    GH-1405 use hashjoin when a group pattern is involved

--- evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/StrictEvaluationStrategy.java
+++ evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/StrictEvaluationStrategy.java
@@ -818,7 +818,7 @@ public class StrictEvaluationStrategy implements EvaluationStrategy, FederatedSe
 			return new ServiceJoinIterator(leftIter, (Service) join.getRightArg(), bindings, this);
 		}
 
-		if (TupleExprs.containsSubquery(join.getRightArg())) {
+		if (TupleExprs.isGraphPatternGroup(join.getRightArg()) || TupleExprs.containsSubquery(join.getRightArg())) {
 			return new HashJoinIteration(this, join, bindings);
 		} else {
 			return new JoinIterator(this, join, bindings);
